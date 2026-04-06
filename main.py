98a2f850f9c0a18d13eb026d1ace6e8783baa6a7
Al Mam <almam@Als-MacBook-Air.local>
2026-04-06 21:42:32 +0300

    Add health endpoint for Render

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from tg_bot.bot import app  # use same variable name as bot.py
+import threading
+import os
+from flask import Flask
+from tg_bot.bot import app
+
+web = Flask(__name__)
+
+@web.route('/')
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    web.run(host="0.0.0.0", port=port)
+
+def run_bot():
+    app.run_polling()
 
 if __name__ == "__main__":
-    print("Starting WeatherBot...")
-    app.run_polling()
+    t1 = threading.Thread(target=run_bot)
+    t1.start()
+
+    run_web()
